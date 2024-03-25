e84c58900b03f3e9952292630e96669fe96ddd30
EC2 Default User <ec2-user@ip-172-31-27-70.us-west-2.compute.internal>
2024-03-25 15:43:07 +0000

    update demo_ui

--- web_ui/demo_ui.py
+++ web_ui/demo_ui.py
@@ -7,9 +7,9 @@ import pandas as pd
 
 threshold = 0.95
 
-def get_protential_tags(url,protential_tags,classification_invoke_url,endpoint_name):
+def get_protential_tags(image_url,protential_tags,classification_invoke_url,endpoint_name):
     url = classification_invoke_url + '/image_classification?'
-    url += ('&url='+url)
+    url += ('&url='+image_url)
     url += ('&protential_tags='+protential_tags)
     url += ('&endpoint_name='+endpoint_name)
     
@@ -17,7 +17,7 @@ def get_protential_tags(url,protential_tags,classification_invoke_url,endpoint_n
     response = requests.get(url)
     result = response.text
     result = json.loads(result)
-
+    print("result:",result)
     tag_confidentials = result['tag_confidentials']
     
     return tag_confidentials
@@ -27,20 +27,6 @@ def get_protential_tags(url,protential_tags,classification_invoke_url,endpoint_n
 def new_image() -> None:
     st.session_state.url = ''
     st.session_state.protential_tags = ''
-    
-    
-def get_image_category(classification_invoke_url,classification_endpoint) -> None:
-    if len(st.session_state.url) ==0:
-        return "url is None"
-    elif len(st.session_state.protential_tags) == 0: 
-        return "protential tags is None"
-    elif len(classification_invoke_url) == 0: 
-        return "classification invoke url is None"
-    elif len(classification_endpoint) == 0:
-        return "classification sagemaker endpoint is None"
-        
-    tag_confidentials = get_protential_tags(url,protential_tags,classification_invoke_url,endpoint_name)
-    return tag_confidentials
     
     
 # Sidebar info
@@ -65,12 +51,18 @@ st.session_state.url = st.text_input(label="Please input image URL", value="")
 st.session_state.protential_tags = st.text_input(label="Please input protential tags,such as: cats,dogs,birds,trees", value="")
 
 if st.button('Get Image Category'):
-    tag_confidentials = get_image_category(classification_invoke_url,classification_endpoint) 
-    if tag_confidentials.find('None') >= 0:
-        st.write(tag_confidentials)
+    if len(st.session_state.url) ==0:
+        st.write("Image url is None")
+    elif len(st.session_state.protential_tags) == 0:
+        st.write("protential tags is None")
+    elif len(classification_invoke_url) == 0:
+        st.write("classification invoke url is None")
+    elif len(classification_endpoint) == 0:
+        st.write("classification sagemaker endpoint is None")
     else:
+
+        tag_confidentials = get_protential_tags(st.session_state.url,st.session_state.protential_tags,classification_invoke_url,classification_endpoint) 
         st.write('tag confidentials')
-        tag_confidentials = json.loads(tag_confidentials)
         category = [tag for tag in tag_confidentials if tag_confidentials[tag] > threshold]
         
         tags = list(tag_confidentials.keys())
